918ada46025c5b9d614f3281e5ee0738c4584b67
IGendrot <igendrot@epita.fr>
2025-11-23 16:35:34 +0100

    Add tests for ClientService

--- tests/Scheduler.Application.Tests/Clients/ClientServiceTests.cs
+++ tests/Scheduler.Application.Tests/Clients/ClientServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AwesomeAssertions;
 using Moq;
 using Scheduler.Application.Clients;
+using Scheduler.Application.Clients.Dtos;
 using Scheduler.Data.Clients.Repositories;
 
 namespace Scheduler.Application.Tests.Clients;
@@ -13,7 +14,9 @@ public class ClientServiceTests
     private readonly Guid _clientId;
     private readonly Guid _fakeClientId;
     private readonly string _clientName = "ClientName";
+    private readonly string _clientNewName = "NewName";
     private readonly Client _client;
+    private readonly Client _newClient;
 
     public ClientServiceTests()
     {
@@ -22,9 +25,15 @@ public class ClientServiceTests
 
         _mockClientRepository = new();
         _client = new(_clientId, _clientName);
+        _newClient = new(_clientId, _clientNewName);
 
         _mockClientRepository.Setup(r => r.Get(It.IsAny<Guid>())).Throws(new KeyNotFoundException());
         _mockClientRepository.Setup(r => r.Get(_clientId)).Returns(_client);
+
+        _mockClientRepository.Setup(r => r.Create(_clientName)).Returns(_client);
+
+        _mockClientRepository.Setup(r => r.Update(_clientId, _clientNewName)).Returns(_newClient);
+        _mockClientRepository.Setup(r => r.Update(_fakeClientId, It.IsAny<string>())).Throws(new KeyNotFoundException());
 
         _clientService = new(_mockClientRepository.Object);
     }
@@ -41,6 +50,35 @@ public class ClientServiceTests
     public void Get_WhenIdNotFound_ShouldThrow()
     {
         Action action = () => _clientService.Get(_fakeClientId);
+        action.Should().Throw<KeyNotFoundException>();
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnClients()
+    {
+        IReadOnlyCollection<Client> result = _clientService.GetAll();
+        result.Should().NotBeNullOrEmpty();
+        result.Should().Contain(_client);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnCreatedClient()
+    {
+        Client result = _clientService.Create(new CreateClientDto(_clientName));
+        result.Should().BeEquivalentTo(_client);
+    }
+
+    [Fact]
+    void Update_ShouldReturnUpdatedClient()
+    {
+        Client result = _clientService.Update(_clientId, new UpdateClientDto(_clientNewName));
+        result.Should().BeEquivalentTo(_newClient);
+    }
+
+    [Fact]
+    void Update_WhenIdNotFound_ShouldThrows()
+    {
+        Action action = () => _clientService.Update(_fakeClientId, new UpdateClientDto(_clientNewName));
         action.Should().Throw<KeyNotFoundException>();
     }
 }
