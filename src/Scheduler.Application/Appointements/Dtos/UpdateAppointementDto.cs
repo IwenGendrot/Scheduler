78cdffba91e6314be1392ea274ede857ece0d6a5
IGendrot <igendrot@epita.fr>
2025-12-01 21:29:35 +0100

    Move some logic from Controllers to Services

--- src/Scheduler.Application/Appointements/Dtos/UpdateAppointementDto.cs
+++ src/Scheduler.Application/Appointements/Dtos/UpdateAppointementDto.cs
@@ -1,5 +1,15 @@
-﻿namespace Scheduler.Application.Appointements.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class UpdateAppointementDto(Guid clientId, Guid patientId, DateOnly appointementDate, int hour) : CreateAppointementDto(clientId, patientId, appointementDate, hour)
+namespace Scheduler.Application.Appointements.Dtos;
+
+public class UpdateAppointementDto(Guid id, DateOnly appointementDate, int hour)
 {
+    [Required]
+    public Guid Id { get; set; } = id;
+
+    [Required]
+    public DateOnly AppointementDate = appointementDate;
+
+    [Required, Range(minimum: 9, maximum: 18)]
+    public int Hour { get; set; } = hour;
 }
